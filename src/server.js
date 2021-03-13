e4f5c5035e5a1cfa1a9f68b5381385cab67a1780
souleymane kone <desolo74@gmail.com>
2021-03-13 04:23:19 -0800

    originalpgconnection

--- src/server.js
+++ src/server.js
@@ -3,22 +3,22 @@ const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
 
 
-var client = new pg.Client({
-  user: "emrwbvujwlxffo",
-  password: "4b9b0a57f6fc57f846c77db82847b0310e354c98c4ca4bac57664c3ebf40d976",
-  database: "d9ulrv9qnqc0hm",
-  port: 5432,
-  host: "18.212.112.148",
-  ssl: true
-}); 
-client.connect();
+// var client = new pg.Client({
+//   user: "emrwbvujwlxffo",
+//   password: "4b9b0a57f6fc57f846c77db82847b0310e354c98c4ca4bac57664c3ebf40d976",
+//   database: "d9ulrv9qnqc0hm",
+//   port: 5432,
+//   host: "18.212.112.148",
+//   ssl: true
+// }); 
+// client.connect();
 
 
-// const db = knex({
-//      client: 'pg',
-//      connection: DATABASE_URL,
-//     })
-//     app.set('db', db)
+const db = knex({
+     client: 'pg',
+     connection: DATABASE_URL,
+    })
+    app.set('db', db)
 
 
 app.listen(PORT, () => {
