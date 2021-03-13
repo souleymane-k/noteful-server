0a110f786dd83a796102865c3b094c259c4e6577
souleymane kone <desolo74@gmail.com>
2021-03-13 04:16:43 -0800

    hostchange

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ var client = new pg.Client({
   password: "4b9b0a57f6fc57f846c77db82847b0310e354c98c4ca4bac57664c3ebf40d976",
   database: "d9ulrv9qnqc0hm",
   port: 5432,
-  host: "ec2-54-211-77-238.compute-1.amazonaws.com",
+  host: "18.212.112.148",
   ssl: true
 }); 
 client.connect();
