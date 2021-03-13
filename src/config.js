fdc09117b0a565f33b193e893489cb23f2278906
souleymane kone <desolo74@gmail.com>
2021-03-13 04:13:00 -0800

    pgupdate

--- src/config.js
+++ src/config.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 module.exports = {
     PORT: process.env.PORT || 8001,
     NODE_ENV: process.env.NODE_ENV || 'production',
+    
+    
     DATABASE_URL: process.env.DATABASE_URL || 'postgres://emrwbvujwlxffo:4b9b0a57f6fc57f846c77db82847b0310e354c98c4ca4bac57664c3ebf40d976@ec2-54-211-77-238.compute-1.amazonaws.com:5432/d9ulrv9qnqc0hm',
   
 
--- src/server.js
+++ src/server.js
@@ -2,11 +2,23 @@ const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
 
-const db = knex({
-     client: 'pg',
-     connection: DATABASE_URL,
-    })
-    app.set('db', db)
+
+var client = new pg.Client({
+  user: "emrwbvujwlxffo",
+  password: "4b9b0a57f6fc57f846c77db82847b0310e354c98c4ca4bac57664c3ebf40d976",
+  database: "d9ulrv9qnqc0hm",
+  port: 5432,
+  host: "ec2-54-211-77-238.compute-1.amazonaws.com",
+  ssl: true
+}); 
+client.connect();
+
+
+// const db = knex({
+//      client: 'pg',
+//      connection: DATABASE_URL,
+//     })
+//     app.set('db', db)
 
 
 app.listen(PORT, () => {
