c7816af7097765698a70574af4c5adef972febdf
souleymane kone <desolo74@gmail.com>
2021-03-21 19:48:27 -0700

    Add SSL true

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ const { PORT, DATABASE_URL } = require('./config')
 const db = knex({
      client: 'pg',
      connection: DATABASE_URL,
-     ssl: true
+     SSL: true
     
     })
     app.set('db', db)
