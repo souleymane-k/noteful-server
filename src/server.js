16db6615e0da30bb790f2faa3977efc1e4bc5aeb
souleymane kone <desolo74@gmail.com>
2021-03-21 21:02:20 -0700

    addssl

--- src/server.js
+++ src/server.js
@@ -1,6 +1,25 @@
 const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
+require("dotenv").config();
+
+const knex=require("knex");
+const app =require("./app");
+const { PORT, DATABASE_URL } = require('./config');
+const pg = require("pg");
+pg.defaults.ssl = process.env.NODE_ENV ==="production"?
+{rejectUnauthorized:false}:false
+const db = knex({
+  client:"pg",
+  connection:DATABASE_URL,
+});
+
+app.set("db", db);
+
+app.listen(PORT,() =>{
+  console.log('server listening at http://localhost:${PORT');
+})
+
 
 
 // var client = new pg.Client({
@@ -27,15 +46,15 @@ const { PORT, DATABASE_URL } = require('./config')
 //   pg.defaults.ssl = true;
 
 
-const db = knex({
-     client: 'pg',
-     connection: DATABASE_URL,
-     SSL: true
+// const db = knex({
+//      client: 'pg',
+//      connection: DATABASE_URL,
+     
     
-    })
-    app.set('db', db)
+//     })
+//     app.set('db', db)
 
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+// app.listen(PORT, () => {
+//   console.log(`Server listening at http://localhost:${PORT}`)
+// })
